0cd45036742916e8a79e5dbd9927c449e7d483d6
Jonas Galante <jfgbr2@yahoo.com>
2014-01-16 08:04:17 -0200

    Initialize repository

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+ruby '2.1.0'
+
+gem 'rails', '4.0.2'
+
+# Use mysql as the database for Active Record
+#gem 'mysql2'
+
+group :development do
+   gem 'pg'
+   gem 'rspec-rails', '2.13.1'
+end
+
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sass-rails', '4.0.1'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '4.0.1'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.2'
+
+gem 'execjs'
+gem 'therubyracer', :platforms => :ruby
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Herokuapp::Application.config.secret_key_base = '980716de40a376d40f37a11a0dab195c291bd162b749dd9695460c56828066ac9d8eb1f384d9f9692229a2510be9370f202b9a751f827b6019a7e4b52ddb050c'
